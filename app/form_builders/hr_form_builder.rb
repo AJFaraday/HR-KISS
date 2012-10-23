56ed802ba6387a14554997c781e7a771c0b07675
Andrew <andrew@AJF-work-lap.(none)>
2012-10-23 17:55:38 +0100

    added convenience method for a check box with plus/minus images

--- app/form_builders/hr_form_builder.rb
+++ app/form_builders/hr_form_builder.rb
@@ -2,8 +2,8 @@ class HrFormBuilder < ActionView::Helpers::FormBuilder
 
   def js_check_box(field, opts={})
     # Defaulting image urls
-    opts[:true_image] ? true_image = opts[:true_image] : true_image = "images/tick.png"
-    opts[:false_image] ? false_image = opts[:false_image] : false_image = "images/cross.png"
+    opts[:true_image] ? true_image = opts[:true_image] : true_image = "/images/tick.png"
+    opts[:false_image] ? false_image = opts[:false_image] : false_image = "/images/cross.png"
     # setting variables for re-use
     dom_id = "#{self.object.class.to_s.downcase}_#{field}"
     js_method = "Toggle#{dom_id.classify}"
@@ -11,10 +11,10 @@ class HrFormBuilder < ActionView::Helpers::FormBuilder
     #Building HTML and JS for the page
 
     result = ActiveSupport::SafeBuffer.new( "
-      <img src='/#{true_image}' id='#{dom_id}_true'
+      <img src='#{true_image}' id='#{dom_id}_true'
         #{!box_checked ? "style='display:none;'" : ""} onclick='#{js_method}();'>
       </img>
-      <img src='/#{false_image}' id='#{dom_id}_false'
+      <img src='#{false_image}' id='#{dom_id}_false'
         #{box_checked ? "style='display:none;'" : ""} onclick='#{js_method}();'>
       </img>
       <script type='text/javascript'>
@@ -28,6 +28,10 @@ class HrFormBuilder < ActionView::Helpers::FormBuilder
     result.concat check_box(field,:style => 'display:none;')
 
     return result
+  end
+
+  def plusminus_check_box(field)
+    js_check_box field, {:true_image => '/images/plus.png',  :false_image => '/images/minus.png'}
   end
 
 end
