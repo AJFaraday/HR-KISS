a37cfac5b8614956c07b9397597d320d1ac905bf
Andrew <andrew@AJF-work-lap.(none)>
2012-11-12 16:09:51 +0000

    applied exempt days to absence timings

--- config/environment.rb
+++ config/environment.rb
@@ -17,6 +17,6 @@ end
 
 class Date
   def workday?
-    self.wday != 6 and self.wday != 0
+    self.wday != 6 and self.wday != 0 and ExemptDay.first(:conditions => ['day = ?', self]).nil?
   end
 end
--- test/unit/absence_test.rb
+++ test/unit/absence_test.rb
@@ -252,4 +252,22 @@ class AbsenceTest < ActiveSupport::TestCase
     assert_valid absence
   end
 
+  def test_exepmt_day_not_counted
+    day = ExemptDay.create(:day => Date.parse('1-1-2013'),
+                           :name => 'new years day')
+    assert_valid day
+
+    absence = Absence.create(:start_time => '31-12-2012 00:00',
+                             :end_time => '2-1-2013 00:00',
+                             :reason => 'reason',
+                             :user_id => User.find_by_admin(true).id,
+                             :variety => 'Holiday',
+                             :start_half_day => 'Full Day',
+                             :end_half_day => 'Lunch Time')
+    assert_valid absence
+    assert_equal 13, absence.end_time.hour
+    assert_equal 1.5, absence.days
+
+  end
+
 end
